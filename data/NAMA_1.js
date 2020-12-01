31a78dd37d3db8f90e7393d3df13c94abab0d0ea
Aprizza <56188101+Aprizza@users.noreply.github.com>
2020-12-01 21:37:12 +0700

    Add files via upload

---
+++ data/NAMA_1.js
@@ -0,0 +1,8 @@
+var json_NAMA_1 = {
+"type": "FeatureCollection",
+"name": "NAMA_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.384997723627677, -7.0036075504251 ] } }
+]
+}
